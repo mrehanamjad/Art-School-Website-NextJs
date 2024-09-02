baed1cb31822e9df4d90890c9f2bb9158b10a988
“mrehanamjad” <rehanamjad814@gmail.com>
2024-09-02 09:27:51 -0700

    .

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+function HeroSection() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center overflow-hidden mx-auto py-10 md:py-0 ">
+      <div className="p-4 relative z-10 w-full text-center">
+        <h1>Unleash Your Creativity</h1>
+        <p>
+            
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
