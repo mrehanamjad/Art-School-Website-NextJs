4bf401e6e833fe5d24aeb4fa5b6a902e13cfeff3
“mrehanamjad” <rehanamjad814@gmail.com>
2024-09-03 03:48:24 -0700

    hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen bg-black/[0.96] antialiased bg-gray-[0.02]">
+    <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1>This is now mine</h1> */}
       <HeroSection />
     </main>
